2ae4e2f80405c645c5ba491e44fbfc7c977825d1
Yuri Fernandes <fernandesyuri104@gmail.com>
2025-04-29 23:06:26 -0300

    Considerações finais js e css.

--- src/main.js
+++ src/main.js
@@ -1,26 +1,33 @@
-
 let inputNumero = document.querySelector(".numero");
 let tentativas = 5;
 
 let numeroSecreto;
 let resultado = document.querySelector(".res");
 
-inputNumero.addEventListener("blur", () => {
-    inputNumero.focus();
-});
-
 function gerarNumeroAleatorio(){
-    return parseInt(Math.random() * 10 + 1);
+    return parseInt(Math.random() * 10 + 1);  
 };
 
 document.addEventListener("DOMContentLoaded", () =>{
     numeroSecreto = gerarNumeroAleatorio();
     console.log(`Número Secreto foi gerado com sucesso! ${numeroSecreto}`);
-    
-    let placarTentaivas = document.createElement('h4');
-    placarTentaivas.textContent = `Número de Tentativas ${tentativas}`
-    document.form.appendChild(placarTentaivas);
+
+    inputNumero.focus();
+
+    let containerPlacar = document.querySelector(".container_placar");
+    if(!containerPlacar){
+        let placarTentativas = document.createElement('p');
+        placarTentativas.className = "container_placar";
+        document.body.appendChild(containerPlacar);
+    }; 
 });
+
+function atualizarPlacar() {
+    let placarTentativas = document.querySelector(".container_placar");
+    if (placarTentativas) {
+        placarTentativas.textContent = `Número de tentativas: ${tentativas}`;
+    };
+};
 
 let btnChutar = document.querySelector(".chutar");
 btnChutar.addEventListener("click", () =>{
@@ -31,31 +38,41 @@ btnChutar.addEventListener("click", () =>{
        
        if(numero <= 0 || numero > 10 || inputNumero.value === ""){
         alert("Digite um número entre 1 e 10.");
+        limparMensagemCampo();
+        console.error("Input acima do permitido!");
     
        }else if(numero > numeroSecreto){
-            resultado.textContent = (`O número secreto é menor que ${numero} e o número de tentativas é ${tentativas}`);
+            resultado.textContent = (`O número secreto é menor que ${numero}`);
             limparMensagemCampo();
             tentativas--;
         
         }else if(numero < numeroSecreto){
-            resultado.textContent = (`O número secreto é maior que ${numero} e o número de tentativas é ${tentativas}`);
+            resultado.textContent = (`O número secreto é maior que ${numero}`);
             limparMensagemCampo();
             tentativas--;
         
         }else if(numero == numeroSecreto){
-            resultado.textContent = (`Parabéns! Você acertou e o número de tentativas é ${tentativas}`);
+            resultado.textContent = (`Parabéns! Você acertou!`);
             btnChutar.style.display = "none";
+            
+            document.querySelector("form").style.background = '#A0CD60';
+            document.querySelector('h3').style.color = 'white';
         };
 
+        atualizarPlacar();
+
         if(tentativas == 0){
-            resultado.textContent = "`Que pena... As tentativas acabaram`"
+            resultado.textContent = "`Que pena... As tentativas acabaram`";
             btnChutar.style.display = "none";
-        }
+        };
     };
+
+    inputNumero.focus();
     verificarNumero();
     console.log(numeroSecreto);
 });
 
+atualizarPlacar();
 function limparMensagemCampo(){
     inputNumero.value = "";
     setTimeout(() =>{
@@ -68,7 +85,9 @@ btnReiniciar.addEventListener("click", () => {
     
     resultado.textContent = `Jogo reiniciado com sucesso!`;
     console.log("Reiniciar NumeroSecreto: " + numeroSecreto);
+    
     console.log("Reiniciar Tentatvas: " + tentativas);
+    console.log("jogo reiniciado com sucesso!")
     
     setTimeout(()=>{
         window.location.reload();
